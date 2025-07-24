5e0144a228cfdd5e628e76b93f76f52c473e1b69
Phani-varma8847 <phani20111@gmail.com>
2025-07-24 11:47:26 -0400

    Create flights_data.py

---
+++ flights_data.py
@@ -0,0 +1,8 @@
+# Sample flight data
+
+flights = [
+    {"id": 101, "from": "New York", "to": "Los Angeles", "time": "08:00", "price": 250},
+    {"id": 102, "from": "Chicago", "to": "Miami", "time": "10:30", "price": 180},
+    {"id": 103, "from": "Dallas", "to": "San Francisco", "time": "14:00", "price": 300},
+    {"id": 104, "from": "Seattle", "to": "Boston", "time": "18:15", "price": 270}
+]
